4728ede4e69ac1f1ef1c8fa37f59cf0acb73e430
Abhishek Sharma <abhisheksharma801831@gmail.com>
2025-08-30 12:40:33 +0530

    Deployment Update

--- backend-node/utils/images.js
+++ backend-node/utils/images.js
@@ -1,10 +1,20 @@
 import Tesseract from "tesseract.js";
+import path from "path";
+
+const corePath = path.join(
+  process.cwd(),
+  "node_modules/tesseract.js-core/tesseract-core-simd.js"
+);
 
 export default async function imageExtractor(file) {
   try {
-    const { data } = await Tesseract.recognize(file.buffer, "eng");
+    const { data } = await Tesseract.recognize(file.buffer, "eng", {
+      corePath,
+    });
+
     return { text: data.text };
   } catch (err) {
+    console.error("Tesseract OCR failed:", err);
     return { error: "Failed to extract text from image." };
   }
 }
